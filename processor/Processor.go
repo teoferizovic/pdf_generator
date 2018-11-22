fa44d023c0f5b23cd8148ae5bb019dfbe20ade30
teoferizovic <ferizovic.teo@gmail.com>
2018-11-22 15:07:44 +0100

    Send post request to external api.

--- processor/Processor.go
+++ processor/Processor.go
@@ -1,8 +1,10 @@
 package processor
 
 import (
+	"bytes"
 	"encoding/json"
 	"github.com/jung-kurt/gofpdf"
+	"net/http"
 	"pdf_generator/model"
 	"strconv"
 )
@@ -14,6 +16,12 @@ func GenerateMsg(msg string,pdfFilePath string)  error{
 	err := json.Unmarshal([]byte(msg), &order)
 
 	if err != nil{
+		return err
+	}
+
+	err = PostRequest(order)
+
+	if err != nil {
 		return err
 	}
 
@@ -39,5 +47,34 @@ func GenerateMsg(msg string,pdfFilePath string)  error{
 	}
 
 	return nil
+}
+
+func PostRequest(order model.Order) error {
+
+	url := "http://127.0.0.1:8000/forder/create"
+
+	var jsonStr = []byte(`{"user_id":`+strconv.FormatInt(order.User_id, 10)+`,"order_id":`+strconv.FormatInt(order.Id, 10)+`,"payment_id":1,"final_price":`+strconv.FormatFloat(order.Final_price, 'f', 2, 64)+`,"created_at":"`+order.Created_at+`"}`)
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+	req.Header.Set("X-Custom-Header", "myvalue")
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	if resp.Status == "201" {
+		return nil
+	}
+
+	return err
+
+	//fmt.Println("response Status:", resp.Status)
+	/*fmt.Println("response Headers:", resp.Header)*/
+	//body, _ := ioutil.ReadAll(resp.Body)
+	//fmt.Println("response Body:", string(body))
 }
 
